2eea7e7773aee90ff13312dc6a6d617501f7eaa9
mam <mam@mach.de>
2020-01-03 09:19:29 +0100

    Aufräumen

--- src/test/java/net/moewes/rest/TestResourceTest.java
+++ src/test/java/net/moewes/rest/TestResourceTest.java
@@ -4,27 +4,24 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class TestResourceTest {
-
+/*
   @Test
   void sayHello() {
-    given().
-        when().get("/api/test/static")
-        .then()
-        .statusCode(200)
-        .body(is("Hello World"));
+    given().when().get("/api/test/static").then().statusCode(200).body(is("Hello World"));
   }
 
   @Test
   void sayHelloCdi() {
 
-    given().
-        when().get("/api/test/cdi")
-        .then()
-        .statusCode(200)
-        .body(is("Hello from CDI"));
+    ValidatableResponse then = given().when().get("/api/test/cdi").then();
+
+    then.statusCode(200).body(is("Hello from CDI"));
   }
+  
+ */
 }
